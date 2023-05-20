7c300bc18068ad83bbad75fd7fb5c5b3101a7c29
Akann2 <eliteskrillex1@gmail.com>
2023-05-20 12:15:24 +0300

    favicon

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const cactus = document.getElementById("cactus");
 let height = 60;
 
 function growCactus() {
-  const startDate = new Date("2022-05-10T21:00:00.000Z");
+  const startDate = new Date("2023-05-08T21:00:00.000Z");
   var timeDiff = Math.abs(Date.now() - startDate.getTime());
   var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
   height += 10;
