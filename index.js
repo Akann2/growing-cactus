9db44ff6eb2ece498d0cf8589ac66497bd63a387
Akann2 <eliteskrillex1@gmail.com>
2024-10-03 21:15:47 +0300

    MEGA UPD

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const cactus = document.getElementById("cactus");
+const heightpx = document.getElementById("height");
 let height = 60;
 
 function growCactus() {
@@ -6,6 +7,7 @@ function growCactus() {
   var timeDiff = Math.abs(Date.now() - startDate.getTime());
   var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
   height += 10;
+  heightpx.innerText  = (diffDays * 10) + 'px'
   cactus.style.height = (diffDays * 10) + 'px';
 }
 
